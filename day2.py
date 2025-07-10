d7a083014aa218ac32e506d84f25d32bb0b5fc30
satyam2004-hub <satyam1sapkota@gmail.com>
2025-07-10 13:51:16 +0545

    July 10 Professional class Task

---
+++ day2.py
@@ -0,0 +1,20 @@
+# string data types
+# lists and tuples
+# conditionals
+
+# slicing
+
+name = "Satyam"
+print(name[0:3])
+
+
+#length
+print(len(name))
+
+#find and replace
+print(name.find("Satyam"))
+replaced = name.replace("Satyam","Aditi")
+print(replaced)
+
+text="My Name is Satyam \nand\ni am 21 years old"
+print(text)
---
+++ day2task.py
@@ -0,0 +1,15 @@
+#write a program to format the following letter using espace sequence character
+
+letter="Dear students,\nThis python course is going well.\nThanks!"
+print(letter)
+
+#replace the double space from problem 3 with single spaces
+name="Satyam is a  good boy"
+print(name.find("  "))
+replaced=name.replace("  "," ")
+print(replaced)
+
+#write a program to detect double space in a string
+
+name="Satyam is a  good boy"
+print(name)
